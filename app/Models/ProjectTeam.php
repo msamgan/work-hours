f43e1f97a7484a598191720c605741eb36453405
msamgan <mohdsamgankhan@gmail.com>
2025-07-21 14:14:40 -0400

    added ProjectTeam model with fillable attributes.

---
+++ app/Models/ProjectTeam.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectTeam extends Model
+{
+    protected $table = 'project_team';
+
+    protected $fillable = [
+        'project_id',
+        'member_id',
+        'is_approver',
+    ];
+}
